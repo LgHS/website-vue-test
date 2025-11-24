d0c5cae9fd6a7adbc26465168fdaa6a575c1a79a
Marcompiler <marcompiler@outlook.com>
2025-11-24 19:56:36 +0100

    Base routes, inspired from LgHS website

--- website/src/App.vue
+++ website/src/App.vue
@@ -1,11 +1,22 @@
 <script setup></script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <header>
+    <img alt="Vue logo" class="logo" src="@/assets/img/logo.svg" />
+
+    <div class="wrapper">
+      <h1>The LgHS Website in Vue !</h1>
+
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/agenda">Agenda</RouterLink>
+        <RouterLink to="/contact">Contact</RouterLink>
+        <RouterLink to="/faq">FAQ</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
 </template>
 
 <style scoped></style>
--- website/src/router/index.js
+++ website/src/router/index.js
@@ -1,8 +1,30 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView,
+    },
+    {
+      path: '/agenda',
+      name: 'agenda',
+      component: () => import('../views/AgendaView.vue'),
+    },
+    {
+      path: '/contact',
+      name: 'contact',
+      component: () => import('../views/ContactView.vue'),
+    },
+    {
+      path: '/faq',
+      name: 'faq',
+      component: () => import('../views/FaqView.vue'),
+    },
+  ],
 })
 
 export default router
