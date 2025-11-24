a214701a8660e6c4a095ccd36acbc91059a4bbff
Marcompiler <marcompiler@outlook.com>
2025-11-24 20:48:45 +0100

    LgHS website style applied to FaqView

--- website/src/views/FaqView.vue
+++ website/src/views/FaqView.vue
@@ -1,14 +1,22 @@
 <script setup>
-  import faq from '@/assets/datas/faq-data.json'
+import faq from '@/assets/datas/faq-data.json'
 </script>
 
 <template>
-  <h1>FAQ</h1>
+  <h3 class="bg-black text-white uppercase font-bold px-4 py-3 text-base mb-4">
+    Questions fréquentes
+  </h3>
 
   <div>
-    <div v-for="item in faq" :key="item.question">
-      <h3>{{ item.question }}</h3>
-      <p v-html="item.answer"></p>
+    <div v-for="item in faq" :key="item.question" class="faq-item">
+      <button class="faq-question">
+        <span>{{ item.question }}</span>
+      </button>
+      <div class="faq-answer">
+        <div>
+          <p v-html="item.answer"></p>
+        </div>
+      </div>
     </div>
   </div>
 </template>
