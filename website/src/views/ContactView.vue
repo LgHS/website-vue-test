892276bd323c5667bf116fb9613cd482a28217a8
Marcompiler <marcompiler@outlook.com>
2025-11-24 20:48:10 +0100

    Contact static content took from LgHS website

--- website/src/views/ContactView.vue
+++ website/src/views/ContactView.vue
@@ -1,3 +1,42 @@
 <template>
-  <h1>Contact</h1>
+  <article class="mb-6">
+    <h3 class="bg-black text-white uppercase font-bold px-4 py-3 text-base mb-4">
+      Contact
+    </h3>
+    <div class="leading-relaxed">
+      <div class="text-center my-6 py-4 bg-gray-50 border-2 border-black">
+        <p class="font-bold mb-1">Liège Hackerspace ASBL</p>
+        <p class="text-sm mb-1">BCE: BE0649.448.256</p>
+        <p class="text-sm">Rue de la Loi 16 bis,<br>4020 Liège, Belgique</p>
+      </div>
+      <p class="mb-4">
+        Pour rappel, le hackerspace n'est pas un club de <i>"pirates informatiques"</i>. Nous ne réalisons aucune
+        activité illégale, et il est inutile de nous contacter pour récupérer un compte, contourner une protection, ou
+        "craquer" quoi que ce soit. Notre mission porte sur l'apprentissage, le partage d'un lieu et de connaissances.
+      </p>
+      <hr class="mb-4">
+      <h4>Général</h4>
+      <p class="mb-4"><b>Préférez toujours le mail général</b> pour nous joindre.
+      </p>
+      <ul>
+        <li><b>Mail général:</b> ping@lghs.be</li>
+        <li><b>Téléphone:</b> +32 (0) 4 287 32 26</li>
+      </ul>
+      <p></p>
+      <h4>Social</h4>
+      <ul>
+        <li><b>Facebook:</b> <a href="https://www.facebook.com/liegehackerspace/" target="_blank"
+            rel="noopener">facebook.com/liegehackerspace/</a></li>
+        <li><b>Instagram:</b> <a href="https://www.instagram.com/lghackerspace/" target="_blank"
+            rel="noopener">instagram.com/lghackerspace/</a></li>
+      </ul>
+      <h4>Autres</h4>
+      <p><em>Requêtes uniquement, nous n'avons pas besoin de consultants, services, ...</em></p>
+      <ul>
+        <li><b>Admin:</b> admin@lghs.be</li>
+        <li><b>Caméra &amp; RGPD:</b> cctv@lghs.be</li>
+        <li><b>NOC &amp; Abuse:</b> noc@lghs.be</li>
+      </ul>
+    </div>
+  </article>
 </template>
