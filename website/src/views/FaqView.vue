3cadfee9fcc0d40a00dbdca2410baea6ab24c0f3
Marcompiler <marcompiler@outlook.com>
2025-11-24 20:03:33 +0100

    Took back FAQ data and added in FaqView

--- website/src/views/FaqView.vue
+++ website/src/views/FaqView.vue
@@ -1,3 +1,14 @@
+<script setup>
+  import faq from '@/assets/datas/faq-data.json'
+</script>
+
 <template>
   <h1>FAQ</h1>
+
+  <div>
+    <div v-for="item in faq" :key="item.question">
+      <h3>{{ item.question }}</h3>
+      <p v-html="item.answer"></p>
+    </div>
+  </div>
 </template>
