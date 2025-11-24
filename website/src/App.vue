51481d545ff4a0a900a7375e9f11f4d531fa62f2
Marcompiler <marcompiler@outlook.com>
2025-11-24 20:46:05 +0100

    RouterLinks with loop and some CSS from LgHS website

--- website/src/App.vue
+++ website/src/App.vue
@@ -1,17 +1,22 @@
-<script setup></script>
+<script setup>
+const links = [
+  { label: "Home", to: "/" },
+  { label: "Agenda", to: "/agenda" },
+  { label: "Contact", to: "/contact" },
+  { label: "FAQ", to: "/faq" },
+]
+</script>
 
 <template>
   <header>
     <img alt="Vue logo" class="logo" src="@/assets/img/logo.svg" />
 
     <div class="wrapper">
-      <h1>The LgHS Website in Vue !</h1>
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/agenda">Agenda</RouterLink>
-        <RouterLink to="/contact">Contact</RouterLink>
-        <RouterLink to="/faq">FAQ</RouterLink>
+      <nav class="flex flex-wrap justify-center md:justify-end gap-2">
+        <RouterLink v-for="link in links" :key="link.to" :to="link.to"
+          class="inline-block uppercase font-bold px-2 py-2 hover:bg-black hover:text-white transition-colors">
+          {{ link.label }}
+        </RouterLink>
       </nav>
     </div>
   </header>
