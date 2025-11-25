04fbffc3cfbcfd4edf121c044a727cd3e331dac7
Marcompiler <marcompiler@outlook.com>
2025-11-25 07:18:12 +0100

    Header added

--- website/src/App.vue
+++ website/src/App.vue
@@ -1,27 +1,10 @@
 <script setup>
 import FooterLayout from './components/layout/FooterLayout.vue';
-
-const links = [
-  { label: "Home", to: "/" },
-  { label: "Agenda", to: "/agenda" },
-  { label: "Contact", to: "/contact" },
-  { label: "FAQ", to: "/faq" },
-]
+import HeaderLayout from './components/layout/HeaderLayout.vue';
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/img/logo.svg" />
-
-    <div class="wrapper">
-      <nav class="flex flex-wrap justify-center md:justify-end gap-2">
-        <RouterLink v-for="link in links" :key="link.to" :to="link.to"
-          class="inline-block uppercase font-bold px-2 py-2 hover:bg-black hover:text-white transition-colors">
-          {{ link.label }}
-        </RouterLink>
-      </nav>
-    </div>
-  </header>
+  <HeaderLayout />
 
   <RouterView />
 
---
+++ website/src/components/layout/HeaderLayout.vue
@@ -0,0 +1,27 @@
+<script setup>
+const links = [
+  { label: "Home", to: "/" },
+  { label: "Agenda", to: "/agenda" },
+  { label: "Contact", to: "/contact" },
+  { label: "FAQ", to: "/faq" },
+]
+</script>
+
+<template>
+  <header class="py-12 relative">
+    <div class="grid grid-cols-1 md:grid-cols-2 gap-8">
+      <div class="flex items-center justify-center md:justify-start">
+        <a href="/" title="Retour à la page d'accueil" class="block">
+          <img src="@/assets/img/logo.svg" alt="Liège Hackerspace - Learn \ Make \ Share" class="h-32 w-auto">
+        </a>
+      </div>
+
+      <nav class="flex items-center justify-center md:justify-end mt-8 md:mt-0">
+        <RouterLink v-for="link in links" :key="link.to" :to="link.to"
+          class="inline-block uppercase font-bold px-2 py-2 hover:bg-black hover:text-white transition-colors">
+          {{ link.label }}
+        </RouterLink>
+      </nav>
+    </div>
+  </header>
+</template>
